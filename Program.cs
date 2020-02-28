f9c2e0c0b3127d57b2837566dec709dad608704b
JerecTharen <kf7ags@gmail.com>
2020-02-27 19:38:41 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pcg_teams_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
